2bdb3851177eab5bcc438e6bcf0ee577492a7075
omarmoashraf <omarmoashraf05@gmail.com>
2026-09-05 00:59:06 +0300

    feat: add IP whitelisting bypass and SLF4J observability
    - Inject whitelisted IPs from application.properties
    - Sanitize IPs at startup using @PostConstruct
    - Implement O(1) bypass logic in interceptor
    - Add detailed warn/debug logs with HTTP method and URI
    
    Closes #1

--- src/main/java/com/omar/gatekeeper/GatekeeperApplication.java
+++ src/main/java/com/omar/gatekeeper/GatekeeperApplication.java
@@ -2,12 +2,15 @@ package com.omar.gatekeeper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+// @SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class GatekeeperApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatekeeperApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(GatekeeperApplication.class, args);
+    }
 
 }
--- src/main/java/com/omar/gatekeeper/config/WebConfig.java
+++ src/main/java/com/omar/gatekeeper/config/WebConfig.java
@@ -1,4 +1,22 @@
 package com.omar.gatekeeper.config;
 
-public class Config {
+import com.omar.gatekeeper.interceptor.RateLimitInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig  implements WebMvcConfigurer {
+    private final RateLimitInterceptor rateLimitInterceptor;
+
+    @Autowired // inject the interceptor
+    public WebConfig(RateLimitInterceptor rateLimitInterceptor){
+        this.rateLimitInterceptor = rateLimitInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(rateLimitInterceptor);
+    }
 }
